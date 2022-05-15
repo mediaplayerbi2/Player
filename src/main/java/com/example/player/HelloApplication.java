f745051024130115160e0c23ce30380cb42f0e94
mediaplayerbi2 <javaprojectbi2@gmail.com>
2022-05-15 18:26:23 +0300

    * Player's redesign

--- src/main/java/com/example/player/HelloApplication.java
+++ src/main/java/com/example/player/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("player.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 258);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Music Player");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/player/HelloController.java
+++ src/main/java/com/example/player/HelloController.java
@@ -1,44 +1,50 @@
 package com.example.player;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-
-import java.net.URI;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.Slider;
 
-public class HelloController implements Initializable {
+public class HelloController {
 
-    private ActionEvent event;
+    @FXML
+    private ResourceBundle resources;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<?> ListView;
+
+    @FXML
+    private Button nextButton;
+
+    @FXML
+    private Button pauseButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button previousButton;
+
+    @FXML
+    private ProgressBar songProgressBar;
+
+    @FXML
+    private ComboBox<String> speedBox;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    void initialize() {
+
 
     }
-    public void playMedia() {
 
-    }
-    public void pauseMedia() {
-
-    }
-    public void resetMedia() {
-
-    }
-    public void previousMedia() {
-
-    }
-    public void nextMedia() {
-
-    }
-    public void changeSpeed(ActionEvent event) {
-
-    }
-    public void beginTimer() {
-
-    }
-    public void cancelTimer() {
-
-    }
 }
