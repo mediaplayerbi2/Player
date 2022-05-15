e6f79aaa79cdbf43abaeac206f12434f410212aa
mediaplayerbi2 <javaprojectbi2@gmail.com>
2022-05-15 23:29:25 +0300

    * add music *change design

--- src/main/java/com/example/player/HelloController.java
+++ src/main/java/com/example/player/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.player;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -8,8 +9,14 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 
 public class HelloController {
+
+
 
     @FXML
     private ResourceBundle resources;
@@ -28,6 +35,10 @@ public class HelloController {
 
     @FXML
     private Button playButton;
+
+
+
+
 
     @FXML
     private Button previousButton;
